2cb64f6140a4ba69e01e6c4b0ad05895ce6294e4
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-09-13 19:41:47 +0530

    add rules card

---
+++ main.js
@@ -0,0 +1,8 @@
+let actions = document.querySelector(".action");
+let ruleButton = document.querySelector(".rules");
+
+ruleButton.addEventListener("click", toggelModal);
+
+// function toggelModal(){
+//     actions.append()
+// } 

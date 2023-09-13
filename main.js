59d12bf5d2bcf4b267c2462bb856cdebca784898
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-09-14 01:07:19 +0530

    modified ui on sumitting

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const rulesButtons = document.querySelectorAll(".toggle-rules");
 const gameKeys = document.querySelectorAll(".game-key");
 const playGround = document.querySelector(".playground");
+const playAgainButton = document.querySelector(".play-again");
 
 rulesButtons.forEach((elem) => {
   elem.addEventListener("click", toggelCard);
@@ -24,7 +25,7 @@ function drawHand(key) {
   console.log(compHand);
 
   const data = compareHands(selectedHand[1], compHand);
-  showResult(data);
+  showResult(data, selectedHand[1], compHand);
 }
 
 function getRandomInt(max) {
@@ -33,21 +34,44 @@ function getRandomInt(max) {
 
 function compareHands(handOne, handTwo) {
   if (handOne == handTwo) {
-    return "tie";
+    return "TIE UP";
   }
 
   if (handOne == "rock") {
-    return handTwo == "scissors" ? "you win" : "you lose";
+    return handTwo == "scissors" ? "YOU WIN" : "YOU LOSE";
   } else if (handOne == "paper") {
-    return handTwo == "rock" ? "you win" : "you lose";
+    return handTwo == "rock" ? "YOU WIN" : "YOU LOSE";
   } else if (handOne == "scissors") {
-    return handTwo == "paper" ? "you win" : "you lose";
+    return handTwo == "paper" ? "YOU WIN" : "YOU LOSE";
   }
 }
 
-function showResult(outcome) {
-  playGround.innerHTML = 
-  `
+function showResult(outcome, selectedHand, compHand) {
+  playGround.innerHTML = `
+  <div class="result">
+  <div class="hand">
+    <h2>YOU PICKED</h2>
+    <div class="key ${selectedHand} ${
+    outcome == "YOU WIN" ? "winner-key" : ""
+  }">
+      <img src="/${selectedHand}.png" alt="" />
+    </div>
+  </div>
+
+  <div class="message">
     <h1>${outcome}</h1>
+    <h2>${outcome == "TIE UP" ? "" : "AGAINST COMPUTER"}</h2>
+    <button class="btn play-again">${
+      outcome == "TIE UP" ? "REPLAY" : "PLAY AGAIN"
+    }</button>
+  </div>
+
+  <div class="hand">
+    <h2>COMPUTER PICKED</h2>
+    <div class="key ${compHand} ${outcome == "YOU LOSE" ? "winner-key" : ""}">
+      <img src="/${compHand}.png" alt="" />
+    </div>
+  </div>
+</div>
   `;
 }
